4bad8ff328b039d852c3bf7572453b09e276c95f
Ivan <iat1@discente.ifpe.edu.br>
2024-08-18 17:26:51 -0300

    adicionando todos os atributos das classes de entidade

--- src/entities/GymMembership.java
+++ src/entities/GymMembership.java
@@ -2,4 +2,19 @@ package entities;
 
 public class GymMembership {
 
+	private String name;
+	private String CPF;
+	private String instructorName;
+	private String[][] trainList;
+	private String gymName;
+	
+	public GymMembership(String name, String cPF, String instructorName, String[][] trainList, String gymName) {
+		this.name = name;
+		CPF = cPF;
+		this.instructorName = instructorName;
+		this.trainList = trainList;
+		this.gymName = gymName;
+	}
+	
+	
 }
