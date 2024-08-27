433b571227cfab9c78c3256e37d510a1d1de860d
Ivan <iat1@discente.ifpe.edu.br>
2024-08-27 15:37:12 -0300

    corrigido lançamento de exceção do cnpj

---
+++ src/model/exceptions/CnpjRangeException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class CnpjRangeException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public CnpjRangeException(String message) {
+		super(message);
+	}
+}
