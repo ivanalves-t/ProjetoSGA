67b0575415ad28eb3c3bdb248bc4a07b77ffc072
Ivan <iat1@discente.ifpe.edu.br>
2024-09-01 13:14:31 -0300

    Criado exceção de plano de adesão

---
+++ src/model/exceptions/CreatePlanException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class CreatePlanException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public CreatePlanException(String message) {
+		super(message);
+	}
+}
