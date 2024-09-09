06b1d2a60f86a944300d8e9e43daf6fe7f761c0a
Ivan <iat1@discente.ifpe.edu.br>
2024-09-09 17:30:49 -0300

    new Name and password exception logic

---
+++ src/model/exceptions/RangeNameException.java
@@ -0,0 +1,11 @@
+package model.exceptions;
+
+public class RangeNameException extends NameException{
+
+
+	private static final long serialVersionUID = 1L;
+
+	public RangeNameException(String message) {
+		super(message);
+	}
+}
